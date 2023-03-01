780f4db5a49045d247cf5857a633fbcd8ac530cc
lijun <lijun28@asiainfo.com>
2023-03-01 14:24:19 +0800

    fix: cascader component perfect.

--- package/cascader/index.js
+++ package/cascader/index.js
@@ -22,10 +22,28 @@ export default createComponent({
         return {
             open: false,
             treeDeepNums: [],
-            resultNums: []
+            resultNums: [],
+            resultStr: ""
         };
     },
-    computed: {
+    mounted() {
+        this.initCompData();
+    },
+    methods: {
+        /** 初始化节点 */
+        initCompData() {
+            if (this.resultNums.length) {
+                let mock = {
+                    children: [...this.data]
+                };
+                this.resultNums.map((item, index) => {
+                    this.treeDeepNums.push(mock);
+                    mock =  mock.children[item];
+                });
+                this.resStrValue();
+            }
+        },
+        /** 选中值回调 */
         resStrValue() {
             let str = "";
             let mock = {};
@@ -34,11 +52,8 @@ export default createComponent({
                 mock = index === 0 ? this.data[item] : mock.children[item];
                 str += (index === 0 ? "" : "/") + mock.label;
             });
-            return str;
-        }
-    },
-
-    methods: {
+            this.resultStr = str;
+        },
         openCascader() {
             this.open = !this.open;
             if (this.open && this.data.length) {
@@ -51,6 +66,7 @@ export default createComponent({
                 }
             }
         },
+        /** 选项点击 */
         dataItemClick(item, index, idx) {
             if (index + 1 < this.treeDeepNums.length) {
                 this.treeDeepNums.splice(index + 1);
@@ -59,10 +75,12 @@ export default createComponent({
             if (item.children) {
                 this.treeDeepNums.push(item);
                 this.resultNums.push(-1);
-            } else {
-                this.open = false;
             }
             this.$set(this.resultNums, index, idx);
+            if (!item.children) {
+                this.resStrValue();
+                this.open = false;
+            }
         },
         getBaseItem(data, parentIndex) {
             return (
@@ -98,7 +116,7 @@ export default createComponent({
                         class={[bem("input")]}
                         placeholder="请选择"
                         readonly
-                        value={this.resStrValue}
+                        value={this.resultStr}
                     />
                     {this.value ? (
                         <span onClick={e => this.deleteValue(e)}>
