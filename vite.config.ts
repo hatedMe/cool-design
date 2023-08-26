b01abff46217fd02b176b56402af90f605c34d39
jiangmin <7548764@qq.com>
2023-08-26 14:54:10 +0800

    config: update vite config  jsx tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vitePluginJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), vitePluginJsx()],
     build: {
         rollupOptions: {
             external: ['vue'],
